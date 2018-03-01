3bef1430b8ef8f4a209dbc505e38988faaa48818
mzwandile.mahlangu <mzwandile.mahlangu@entelect.co.za>
2018-03-01 21:48:36 +0200

    adding total Ride distance function

--- HashCode2018/src/Transportation.java
+++ HashCode2018/src/Transportation.java
@@ -17,6 +17,14 @@ public class Transportation {
         return false;
     }
 
+    public int getTotalRideDistance(Vehicle vehicle, Ride ride) {
+        int vehicleAndStartDistance = distance(vehicle.getCurrentRow(), ride.getRowStart(),
+                vehicle.getCurrentColumn(), ride.getColumnStart());
+        int rideDistance = distance(ride.getRowStart(), ride.getRowEnd(), ride.getColumnStart(), ride.getColumnEnd());
+        int totalRideDistance = vehicleAndStartDistance + rideDistance;
+        return totalRideDistance;
+    }
+
     public int distance(int startRow, int endRow, int startCol, int endCol) {
         int distance = Math.abs(startRow - endRow) + Math.abs(startCol - endCol);
         return distance;
