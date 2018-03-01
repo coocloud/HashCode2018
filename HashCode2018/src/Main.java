37511307389dfe8ff37a0bb0f6e2abb3c65a93fd
George Fu <kuan-hsiang.fu@entelect.co.za>
2018-03-01 19:04:12 +0200

    Input Processing

--- HashCode2018/src/Main.java
+++ HashCode2018/src/Main.java
@@ -1,6 +1,21 @@
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = null;
+        FileOutputStream out = new FileOutputStream("file/hello.out");
+        try {
+            fis = new FileInputStream("file/hello.in");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis));
+        BufferedWriter writer = null;
+        writer = new BufferedWriter(new OutputStreamWriter(out));
+
+
+        String line = bufferedReader.readLine();
+        String firstLine[] = line.split(" ");
     }
 }
